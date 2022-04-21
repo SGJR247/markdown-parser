db34fb85eb5d14868c13fcdb2363e348d5bc42f3
Sam <sgenous@ucsd.edu>
2022-04-21 14:32:16 -0700

    MarkdownParseTest file added
    javac -cp ".;lib\junit-4.13.2.jar;lib\hamcrest-core-1.3.jar"
    MarkdownParseTest.java
    
    java -cp ".;lib/junit-4.13.2.jar;lib/hamcrest-core-1.3.jar"
    org.junit.runner.JUnitCore MarkdownParseTest

---
+++ MarkdownParseTest.java
@@ -0,0 +1,8 @@
+import static org.junit.Assert.*; // Imports assert methods
+import org.junit.*;               // Imports junit methods
+public class MarkdownParseTest {  // creates new class for MarkdownParseTest
+    @Test                         // Annotation to make following method a test case method
+    public void addition() {      // New method created, called addition
+        assertEquals(2, 1 + 1);   // Determines if expected value (2) and actual value (1 + 1) are equal
+    }    
+}
