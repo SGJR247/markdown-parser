92cb8afead6775170d4c8c7792c0919a4f86532a
Sam <sgenous@ucsd.edu>
2022-04-21 14:53:48 -0700

    2 testLinks methods added

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -1,23 +1,35 @@
-import static org.junit.Assert.*; // Imports assert methods
-
+import static org.junit.Assert.*;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
-import org.junit.*;               // Imports junit methods
+import org.junit.*;                         
 
-public class MarkdownParseTest {  // creates new class for MarkdownParseTest
-    @Test                         // Annotation to make following method a test case method
-    public void addition() {      // New method created, called addition
-        assertEquals(2, 1 + 1);   // Determines if expected value (2) and actual value (1 + 1) are equal
+public class MarkdownParseTest {            
+    @Test                         
+    public void addition() {
+        assertEquals(2, 1 + 1);
     }
     
     @Test
-    public void testGetLinks() throws Exception {
-        String[] expectedLinks = {"https://something.com", "some-thing.html"};
+    public void testGetLinks1() throws Exception {
+        String[] expectedLinks1 = {"https://something.com", "some-thing.html"};
         
         Path fileName = Path.of("test-file.md");
         String content = Files.readString(fileName);
-        ArrayList<String> actualLinks = MarkdownParse.getLinks(content);
-        assertArrayEquals(expectedLinks, actualLinks.toArray()); 
+        ArrayList<String> actualLinks1 = MarkdownParse.getLinks(content);
+        assertArrayEquals(expectedLinks1, actualLinks1.toArray()); 
+    }
+
+    @Test
+    public void testGetLinks2() throws Exception {
+        String[] expectedLinks2 = {"some-thing.html", "https://www.thesaurus.com/", 
+                                  "https://www.dictionary.com/", "https://wordcounter.net/", 
+                                  "https://www.desmos.com/calculator", "wumpa-islands.html", 
+                                  "https://something.com"};
+        
+        Path fileName = Path.of("test-file2.md");
+        String content = Files.readString(fileName);
+        ArrayList<String> actualLinks2 = MarkdownParse.getLinks(content);
+        assertArrayEquals(expectedLinks2, actualLinks2.toArray()); 
     }
 }
