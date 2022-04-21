e3d261ab388ac35483c27b87791f2a55d54a3f0a
Sam <sgenous@ucsd.edu>
2022-04-21 14:48:25 -0700

    testGetLinks method added

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -1,8 +1,23 @@
 import static org.junit.Assert.*; // Imports assert methods
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
 import org.junit.*;               // Imports junit methods
+
 public class MarkdownParseTest {  // creates new class for MarkdownParseTest
     @Test                         // Annotation to make following method a test case method
     public void addition() {      // New method created, called addition
         assertEquals(2, 1 + 1);   // Determines if expected value (2) and actual value (1 + 1) are equal
-    }    
+    }
+    
+    @Test
+    public void testGetLinks() throws Exception {
+        String[] expectedLinks = {"https://something.com", "some-thing.html"};
+        
+        Path fileName = Path.of("test-file.md");
+        String content = Files.readString(fileName);
+        ArrayList<String> actualLinks = MarkdownParse.getLinks(content);
+        assertArrayEquals(expectedLinks, actualLinks.toArray()); 
+    }
 }
