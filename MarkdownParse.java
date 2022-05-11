af67a121defc59d801f39987f0ce9acf82fe6f51
Sam <sgenous@ucsd.edu>
2022-05-10 19:49:25 -0700

    Update MarkdownParse.java

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -43,7 +43,7 @@ public class MarkdownParse {
                 currentIndex = closeParen + 1;
             }
         }
-        return toReturn; //Boom shakalaka
+        return toReturn;
     }
 
 
